ecdb97ad4cb6bd59d316bed20204ab8dad0defa8
David Shinji Furuya <tbzmxm2fyn@privaterelay.appleid.com>
2022-11-05 06:01:07 +0000

    Update index.js

--- src/consts/index.js
+++ src/consts/index.js
@@ -1,5 +1,3 @@
-// const API_KEY = process.env.REACT_APP_HUBSPOT_API_KEY;
-// export const URL = `https://candidate.hubteam.com/candidateTest/v3/problem/dataset?userKey=${API_KEY}`;
-// export const POST_URL = `https://candidate.hubteam.com/candidateTest/v3/problem/result?userKey=${API_KEY}`;
-export const URL = `https://candidate.hubteam.com/candidateTest/v3/problem/dataset?userKey=aeee9b6a5f341ec132b3f8b6e7ec`;
-export const POST_URL = `https://candidate.hubteam.com/candidateTest/v3/problem/result?userKey=aeee9b6a5f341ec132b3f8b6e7ec`;
+const API_KEY = process.env.REACT_APP_HUBSPOT_API_KEY;
+export const URL = `https://candidate.hubteam.com/candidateTest/v3/problem/dataset?userKey=${API_KEY}`;
+export const POST_URL = `https://candidate.hubteam.com/candidateTest/v3/problem/result?userKey=${API_KEY}`;
